c089fbb3c203b8095f1e8bad86d0fd8c6c1040f1
IlhamWakana <113600726+IlhamWakana@users.noreply.github.com>
2025-07-10 09:37:15 +0700

    Add files via upload

--- script/kumpulan_doa/doa2.gd
+++ script/kumpulan_doa/doa2.gd
@@ -1,1 +1,20 @@
-extends Node
+extends Control
+
+# Referensi ke AudioStreamPlayer2D
+@onready var audio = $player_audio
+
+func _on_play_pressed() -> void:
+	if audio.stream_paused:
+		audio.stream_paused = false
+	else:
+		audio.play()
+
+func _on_pause_pressed() -> void:
+	audio.stream_paused = true
+
+func _on_restart_pressed() -> void:
+	audio.stop()
+	audio.play()
+
+func _on_kembali_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/pilihan.tscn") # ganti path sesuai scene utama kamu
