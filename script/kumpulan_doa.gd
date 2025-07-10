83adf6777d2e0236bd0a5048377f6896b2158407
IlhamWakana <113600726+IlhamWakana@users.noreply.github.com>
2025-07-10 09:36:39 +0700

    Add files via upload

--- script/kumpulan_doa.gd
+++ script/kumpulan_doa.gd
@@ -1,16 +1,19 @@
 extends Node
 
 func _on_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/kumpulan_doa/doa1.tscn")
 
 func _on_button_2_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/kumpulan_doa/doa2.tscn")
 
 func _on_button_3_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/kumpulan_doa/doa3.tscn")
 
 func _on_button_4_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/kumpulan_doa/doa4.tscn")
 
 func _on_button_5_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/kumpulan_doa/doa5.tscn")
+
+func _on_buttonkembali_pressed():
+	get_tree().change_scene_to_file("res://scenes/Main.tscn")
